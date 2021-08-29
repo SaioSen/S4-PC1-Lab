4d4c956de94e1869c21c896bc36cf4926729448a
Teresa Atencio <teresa.atencio.robles@hotmail.com>
2021-08-28 22:43:27 -0500

    Agregar Productos

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using S4_PC1_Lab.Models;
+using S4_PC1_Lab.Data;
+
 namespace S4_PC1_Lab.Controllers
 {
     public class ProductoController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
 
         public IActionResult Index()
         {
@@ -19,6 +29,8 @@ namespace S4_PC1_Lab.Controllers
         [HttpPost]  
         public IActionResult Create(Producto objProducto)
         {
+            _context.Add(objProducto);
+            _context.SaveChanges();
             ViewData["Message"] = "Producto Registrado";
             return View("Index");
         }
--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,16 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace S4_PC1_Lab.Models
 {
+    [Table("t_producto")]
     public class Producto
     {
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int id { get; set;}
 
+        [Column("nombre")]
         public string Nombre { get; set;}
 
+        [Column("categoria")]
         public string Categoria { get; set;}
 
+        [Column("precio")]
         public double Precio { get; set;}
 
+        [Column("descuento")]
         public double Descuento { get; set;}
 
     }
